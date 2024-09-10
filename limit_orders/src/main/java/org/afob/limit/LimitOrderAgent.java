8149606e078b0593fd48af24965c1cf9b01a3cc2
Sireesha <kvrlsireeha@gmail.com>
2024-09-10 19:01:48 +0530

    first commit

---
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -0,0 +1,39 @@
+package org.afob.limit;
+
+import org.afob.execution.ExecutionClient;
+import org.afob.prices.PriceListener;
+
+import java.math.BigDecimal;
+
+public class LimitOrderAgent implements PriceListener {
+
+    String productId;
+    BigDecimal price;
+
+
+    ExecutionClient ec;
+
+    public LimitOrderAgent(final ExecutionClient ec) {
+        this.ec=ec;
+    }
+
+    public void limitOrder() throws ExecutionClient.ExecutionException {
+
+        if(productId=="IBM"){
+            if(this.price.compareTo(BigDecimal.valueOf(100))<0){
+                ec.buy(productId,1000*this.price.intValue());
+            }
+        }
+     }
+    @Override
+    public void priceTick(String productId, BigDecimal price)  {
+            this.productId=productId;
+            this.price=price;
+          try {
+              limitOrder();
+          }catch (ExecutionClient.ExecutionException e){
+              System.out.println("exception thrown"+e);
+          }
+    }
+
+}
