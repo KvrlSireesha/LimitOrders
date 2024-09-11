0cdf1ab31b097add1769aeef68f3ccbc80e0fc98
Sireesha <kvrlsireeha@gmail.com>
2024-09-11 11:13:27 +0530

    wrote test cases for LimitOrderAgent

--- limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
+++ limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
@@ -2,11 +2,21 @@ package org.afob.limit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.afob.limit.LimitOrderAgent;
+import java.math.BigDecimal;
 
 public class LimitOrderAgentTest {
 
     @Test
     public void addTestsHere() {
         Assert.fail("not implemented");
+    }
+
+    @Test
+    public void priceTickTest(){
+        String productId="IBM";
+        BigDecimal price=new BigDecimal(100);
+        priceTick( productId, price);
+
     }
 }
