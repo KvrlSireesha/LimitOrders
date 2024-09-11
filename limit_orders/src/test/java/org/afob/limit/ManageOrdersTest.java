d3548aa6d08f781ef0c0e350e5649b1110af8231
Sireesha <kvrlsireeha@gmail.com>
2024-09-11 11:57:36 +0530

    wrote test cases for ManageOrders

--- limit_orders/src/test/java/org/afob/limit/ManageOrdersTest.java
+++ limit_orders/src/test/java/org/afob/limit/ManageOrdersTest.java
@@ -12,20 +12,26 @@ import static org.junit.Assert.assertThrows;
 
 public class ManageOrdersTest {
     @Test
-    public void priceTickTest(){
+    public void addOrdersTest(){
         String productId="IBM";
-        BigDecimal price=new BigDecimal(100);
-        priceTick( productId, price);
+        boolean isBuy=false;
+        BigDecimal amount=new BigDecimal(50);
+        BigDecimal limit=new BigDecimal(100);
 
-    }
+         addOrders( isBuy,productId,amount,limit);
+
+
+        }
 
     @Test
-    public void priceTickTestException(){
+    public void addOrdersTestException(){
         String productId=null;
-        BigDecimal price=new BigDecimal(100);
+        boolean isBuy=false;
+        BigDecimal amount=new BigDecimal(50);
+        BigDecimal limit=new BigDecimal(100);
 
         Exception exception = assertThrows(ExecutionClient.ExecutionException.class, () -> {
-            priceTick( productId, price);
+            addOrders( isBuy,productId,amount,limit);
         });
 
         assertEquals("failed to buy: environment error", exception.getMessage());
