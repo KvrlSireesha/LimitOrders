ffa575c5d17ec1450486bb5bc365c0cefb4d6a6f
Sireesha <kvrlsireeha@gmail.com>
2024-09-11 10:35:21 +0530

    modified  the class LimitOrderAgent

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -7,13 +7,33 @@ import java.math.BigDecimal;
 
 public class LimitOrderAgent implements PriceListener {
 
+    String productId;
+    BigDecimal price;
+
+    ExecutionClient ec;
+
     public LimitOrderAgent(final ExecutionClient ec) {
+        this.ec=ec;
     }
 
+    public void limitOrder() throws ExecutionClient.ExecutionException {
+
+        if(productId=="IBM"){
+            if(this.price.compareTo(BigDecimal.valueOf(100))<0){
+                ec.buy(productId,1000*this.price.intValue());
+            }
+        }
+    }
 
     @Override
-    public void priceTick(String productId, BigDecimal price) {
-
+    public void priceTick(String productId, BigDecimal price)  {
+        this.productId=productId;
+        this.price=price;
+        try {
+            limitOrder();
+        }catch (ExecutionClient.ExecutionException e){
+            System.out.println("exception thrown"+e);
+        }
     }
 
 }
